dab0a2085601bcb34ec2190ae4ffff50367a07f3
dokimion24 <leecy3610@gmail.com>
2025-02-23 20:49:02 +0900

    feat: 로그인화면 UI 구현

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,44 @@
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Signin = () => {
+  const handleLogin = () => {
+    console.log("Login");
+  };
+
   return (
-    <View>
-      <Text>Signin</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-bold text-black-200">
+            Welcome to LIVON
+          </Text>
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-12"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black ml-2">
+                Google 계정으로 로그인
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
