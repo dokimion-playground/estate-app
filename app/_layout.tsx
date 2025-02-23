587ae92be824a136ee7fc323ce91376672f930a9
dokimion24 <leecy3610@gmail.com>
2025-02-23 20:50:16 +0900

    feat: 폰트 설정 구현

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,34 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
 }
